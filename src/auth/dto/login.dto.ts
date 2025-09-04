32facf794063a3ccb1e3d12037e8abb35b65a2a8
Vasyl M. <vasia.motnin@gmail.com>
2025-09-04 19:56:21 +0200

    Enhance LoginDto: Add MinLength validation for password to ensure a minimum length of 8 characters.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({
@@ -18,5 +18,6 @@ export class LoginDto {
   })
   @IsString()
   @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
