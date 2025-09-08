ca96c3840cc89785ca4ca2b38edacdbc4c118fd1
Vasyl M. <vasia.motnin@gmail.com>
2025-09-08 19:05:43 +0200

    Refactor FolderResponseDto: Rename ParentFolder class to Folder and extend FolderResponseDto to inherit from Folder, updating parentFolders property to use Folder type for improved structure and consistency.

--- src/folder/dto/folder-response.dto.ts
+++ src/folder/dto/folder-response.dto.ts
@@ -7,7 +7,7 @@ import {
   IsString,
 } from 'class-validator';
 
-class ParentFolder {
+class Folder {
   @ApiProperty({
     example: 'Folder name',
     description: 'Folder name',
@@ -42,45 +42,12 @@ class ParentFolder {
   updatedAt?: string;
 }
 
-export class FolderResponseDto {
-  @ApiProperty({
-    example: 'Folder name',
-    description: 'Folder name',
-  })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
-  @ApiProperty({
-    example: '550e8400-e29b-41d4-a716-446655440000',
-    description: 'Folder id',
-  })
-  @IsString()
-  @IsNotEmpty()
-  id: string;
-
-  @ApiProperty({
-    example: '2023-01-01T00:00:00Z',
-    description: 'Folder creation date',
-  })
-  @IsDateString()
-  @IsNotEmpty()
-  createdAt: string;
-
-  @ApiProperty({
-    example: '2023-01-01T00:00:00Z',
-    description: 'Folder last update date',
-  })
-  @IsDateString()
-  @IsNotEmpty()
-  @IsOptional()
-  updatedAt?: string;
-
+export class FolderResponseDto extends Folder {
   @ApiProperty({
     example: 'Parent folders',
     description: 'Parent folders',
   })
   @IsArray()
   @IsNotEmpty()
-  parentFolders: ParentFolder[];
+  parentFolders: Folder[];
 }
