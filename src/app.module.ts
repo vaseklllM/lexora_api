8c1f8cf4630db0f6b6ebb81c2bbf794ed7ef6476
Vasyl M. <vasia.motnin@gmail.com>
2025-10-11 23:27:23 +0200

    refactor: comment out medium cache configuration in app module for clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,13 +21,13 @@ import { SettingsModule } from './settings/settings.module';
       {
         name: 'short',
         ttl: 1000,
-        limit: 3,
-      },
-      {
-        name: 'medium',
-        ttl: 10000,
         limit: 20,
       },
+      // {
+      //   name: 'medium',
+      //   ttl: 10000,
+      //   limit: 20,
+      // },
       {
         name: 'long',
         ttl: 60000,
