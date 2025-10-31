57083ed3e79b7f3376886e685d323c18cc02ace1
Vasyl M. <vasia.motnin@gmail.com>
2025-10-31 21:24:44 +0100

    chore: update .env.example with shortened JWT secrets and add VertexProvider to AiModule for improved functionality

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -3,10 +3,11 @@ import { AiController } from './ai.controller';
 import { AiService } from './ai.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { VertexModule } from 'src/vertex/vertex.module';
+import { VertexProvider } from 'src/vertex/vertex';
 
 @Module({
   imports: [DatabaseModule, VertexModule],
   controllers: [AiController],
-  providers: [AiService],
+  providers: [AiService, VertexProvider],
 })
 export class AiModule {}
